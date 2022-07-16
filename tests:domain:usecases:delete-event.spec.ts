00a932ba2c3dfb396d96770f4d3e299527b5ac9d
kenard rodrigues da silva <kenardrs@gmail.com>
2022-07-16 11:27:11 -0300

    ensure DeleteEvent throws error if userId is invalid

--- tests:domain:usecases:delete-event.spec.ts
+++ tests:domain:usecases:delete-event.spec.ts
@@ -1,22 +1,37 @@
+import { type } from "os"
+
 class DeleteEvent {
     constructor( 
         private readonly loadGroupRepository: LoadGroupRepository
     ) {}
 
-    async perform({ id }: {id: string, userId: string}): Promise<void> {
+    async perform({ id, userId }: {id: string, userId: string}): Promise<void> {
         const group = await this.loadGroupRepository.load({ eventId: id})
         if ( group === undefined ) throw new Error()
+        if ( group.users.find(user => user.id === userId) === undefined) throw new Error()
     }
 }
 
 interface LoadGroupRepository {
-    load: (input: { eventId: string }) => Promise<any>
+    load: (input: { eventId: string }) => Promise<Group | undefined>
+}
+
+// Creating as a 'type' instead a class because it does not have behavior yet
+type Group = {
+    users: GroupUser[]
+}
+
+type GroupUser = {
+    id: string
+    permission: string
 }
 
 class LoadGroupRepositoryMock implements LoadGroupRepository {
     eventId?: string
     callsCount = 0
-    output: any = 'any_value'
+    output?: Group = {
+        users: [{ id: 'any_user_id', permission: 'any' }]
+    }
 
     async load({ eventId }: { eventId: string }): Promise<any> {
         this.eventId = eventId
@@ -54,13 +69,27 @@ describe('DeleteEvent', () => {
         expect(loadGroupRepository.callsCount).toBe(1)
     })
 
-    it('should throw error on invalid eventId', async () => {
+    it('should throw error if eventId is invalid', async () => {
         const { sut, loadGroupRepository } = makeSut()
         loadGroupRepository.output = undefined
 
         // how to test exception at asynchronous statements promise
-        // 1 - get promise into a variable
+        // 1 - set promise into a variable
         const promise = sut.perform({id, userId})
+
+        // 2 - test promise await rejects against an generic error throws 
+        await expect(promise).rejects.toThrowError()
+    })
+
+    it('should throw error if userId is invalid', async () => {
+        const { sut, loadGroupRepository } = makeSut()
+        loadGroupRepository.output = {
+            users: [{ id: 'any_user_id', permission: 'any' }]
+        }
+
+        // how to test exception at asynchronous statements promise
+        // 1 - set promise into a variable
+        const promise = sut.perform({id, userId: 'invalid_id'})
 
         // 2 - test promise await rejects against an generic error throws 
         await expect(promise).rejects.toThrowError()
